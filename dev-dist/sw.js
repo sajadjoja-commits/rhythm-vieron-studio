04cbec07499648f4cff3dc36c9582f69e6f9f068
sajadjoja <sajadjoja@gmail.com>
2026-07-26 13:44:17 +0000

    Final updates and service worker sync

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u36aeb1sfb8"
+    "revision": "0.pnufh7tlod8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
